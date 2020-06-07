2394c38007841ad0ece2608ee820042dcab118b3
A-Diliyaer <aji.diliyaer@gmail.com>
2020-06-07 14:00:20 -0400

    first

--- src/test/java/com/vytrack/runners/CreateCarRunner.java
+++ src/test/java/com/vytrack/runners/CreateCarRunner.java
@@ -1,10 +1,10 @@
 package com.vytrack.runners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
 @CucumberOptions(
         glue = "com/vytrack/step_definitions",
         features = "src/test/resources/features/fleet",
@@ -16,6 +16,6 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber2.json"
         }
 )
-public class CreateCarRunner {
+public class CreateCarRunner extends AbstractTestNGCucumberTests {
 
 }
